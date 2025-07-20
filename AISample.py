126d2c0b2b9217e41717afd87f1f78a875b3628c
Hiren Patel <hirenp@hexaware.com>
2025-07-20 16:34:22 +0530

    Added API call and created different functions

--- AISample.py
+++ AISample.py
@@ -1,32 +1,64 @@
 from dotenv import load_dotenv
 import os
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 
-subject = """Rahul Dravid"""
+from third_parties.linkedin import get_linkedin_profile
+
 tag = "information"
 
-if __name__ == "__main__":
-    # This function loads the data from .env file
-    load_dotenv()
-    print("Finding relevant information for "+ subject)
 
+def find_relevant_information():
+    subject = """Rahul Dravid"""
+    print("Finding relevant information for " + subject)
+
+    # Normal data find
     summary_template = "Provide the {" + tag + "}"
 
     summary_prompt_template = PromptTemplate(
         input_variables=[tag], template=summary_template
     )
-    # This is use for live model
-    # llm = ChatOpenAI(temperature=0, model_name="o3-pro")
+
+    llm = ChatOllama(model=os.environ["MODEL"])
+
+    chain = summary_prompt_template | llm | StrOutputParser()
+
+    res = chain.invoke(input={tag: subject})
+
+    print(res)
+
+    print("Found relevant information for " + subject)
+
+
+def find_linkedin_profile():
+    # LinkedIn profile find
+    summary_template = "Provide the LinkedIn profile {" + tag + "}"
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=[tag], template=summary_template
+    )
 
     # This is for local model (Used Ollama)
     llm = ChatOllama(model=os.environ["MODEL"])
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | StrOutputParser()
 
-    res = chain.invoke(input={tag: subject})
+    linkedin_data = get_linkedin_profile(
+        "https://gist.githubusercontent.com/hpdeveloper28/5b66ccb34c2dc02ebea197bfe3d3680e/raw/52020e549c901224d372396b35318957fbb3bd36/sample_linkedin.json"
+    )
 
-    print(res.pretty_print())
+    res = chain.invoke(input={tag: linkedin_data})
 
-    print("Found relevant information for " +subject)
+    print(res)
+
+
+if __name__ == "__main__":
+    # This function loads the data from .env file
+    load_dotenv()
+
+    # This is use for live model
+    # llm = ChatOpenAI(temperature=0, model_name="o3-pro")
+
+    find_relevant_information()
---
+++ third_parties/linkedin.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def get_linkedin_profile(profile_url: str):
+    response = requests.get(profile_url, timeout=20)
+    data = response.json()
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        get_linkedin_profile(
+            "https://gist.githubusercontent.com/hpdeveloper28/5b66ccb34c2dc02ebea197bfe3d3680e/raw/52020e549c901224d372396b35318957fbb3bd36/sample_linkedin.json"
+        )
+    )
